a7921700586fccaa3f511c22a9d40d25ba6ba2ba
Gabriel Mariano <gabrielmarianodejesus@gmail.com>
2022-01-31 20:29:01 -0300

    Projeto criado

---
+++ backend/src/test/java/com/gabrielm/dsmovies/DsmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielm.dsmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
