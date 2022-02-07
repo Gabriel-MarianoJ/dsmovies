ad420136f1bd152ebda0765c546f1fb85996aca1
Gabriel Mariano <gabrielmarianodejesus@gmail.com>
2022-02-06 23:34:18 -0300

    Arrumado Form

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,11 +1,7 @@
 import FormCard from 'components/FormCard';
 import { useParams } from 'react-router-dom';
-import { Movie } from 'types/movie';
 import './form.css';
 
-type Props = {
-    movie: Movie;
-}
 const Form = () => {
 
     const params = useParams();
